45915dba7f6d3850f4cf03966255f51f0d868de1
tzwm <git@tzwm.me>
2023-09-10 16:14:05 +0800

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import 'papercss/dist/paper.min.css';
+  import { Navbar } from 'spaper';
+</script>
+
+<Navbar split={false}>
+  <h3 slot="brand">
+    <a href="/">好捏模型</a>
+  </h3>
+  <ul class="inline">
+    <li><a href="/download">下载</a></li>
+    <li><a href="#">精选</a></li>
+  </ul>
+</Navbar>
+
+<slot></slot>
