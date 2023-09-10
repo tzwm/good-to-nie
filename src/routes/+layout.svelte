a8e158e882d83923ed746a50bb8631f12a0fa29c
tzwm <git@tzwm.me>
2023-09-10 17:24:21 +0800

    fix: wrong nav related path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,27 @@
 <script>
   import 'papercss/dist/paper.min.css';
   import { Navbar } from 'spaper';
+  import {onMount} from 'svelte';
+
+  let hostPath = '';
+  onMount(() => {
+    let path = window.location.href;
+    hostPath = path.substring(0, path.lastIndexOf('/'));
+    console.log(path);
+    console.log(hostPath);
+  });
 </script>
 
 <Navbar split={false}>
   <h3 slot="brand">
-    <a href="/">好捏模型</a>
+    <a href="{hostPath}">好捏模型</a>
   </h3>
   <ul class="inline">
-    <li><a href="/download">下载</a></li>
-    <li><a href="#">精选</a></li>
+    <li><a href="{hostPath}/download">下载</a></li>
+    <li><a href="{hostPath}/featured">精选</a></li>
   </ul>
 </Navbar>
+
+<p>{hostPath}</p>
 
 <slot></slot>
